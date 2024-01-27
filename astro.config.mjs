488d49a778bdf783cc736005a892b931a9e6466e
aconitum3 <admin@example.com>
2024-01-27 11:04:03 +0000

    modified astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://aconitum3.github.io"
+  site: "https://aconitum3.github.io",
   markdown: {
     rehypePlugins: [
       ['rehype-katex',{}]
